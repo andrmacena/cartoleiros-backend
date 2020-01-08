8cac2a465e20d27bc9cc07ef58faad6aef75d349
André Macena <andrmacena_@outlook.com>
2020-01-08 20:51:08 -0300

    Criado database.js default

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+   dialect: 'postgres',
+   url: "<YOUR URL HERE>",
+   host: '<YOUR HOST HERE>',
+   username: '<YOUR USER NAME HERE>',
+   password: '<YOUR PASSWORD HERE>',
+   database: '<YOUR DATABASE HERE>',
+   ssl: true,
+   define: {
+      timestamps: true,
+      underscored: true
+   }
+}

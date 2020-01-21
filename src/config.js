8a90c9048d1a622e95b50e5209e3c22e870752dd
André Macena <andrmacena_@outlook.com>
2020-01-20 22:33:27 -0300

    Adicionado arquivo de configuracao para envio de email e storage da azure

---
+++ src/config.js
@@ -0,0 +1,7 @@
+global.SALT_KEY = '<YOUR SALT KEY HERE>'
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> seja bem vindo à Node Store'
+
+module.exports = {
+    sendgridKey:'<YOUR SENDGRID KEY HERE>',
+    containerConnectionString: '<YOUR AZURE STORAGE KEY HERE>'
+}

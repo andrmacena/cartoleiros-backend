5b62643e53da4f749a9d51b518a74d27d7e04735
André Macena <andrmacena_@outlook.com>
2020-01-20 22:34:03 -0300

    Criado authService.js, contendo validacao, geracao e autenticacao de token

---
+++ src/services/authService.js
@@ -0,0 +1,51 @@
+const jwt = require('jsonwebtoken')
+const config = require('../config/database')
+
+module.exports = {
+    async generateToken(data) {
+        return jwt.sign(data, config.password, { expiresIn: '1d' })
+
+    },
+
+    async decodeToken(token) {
+        var data = await jwt.verify(token, config.password)
+
+        return data
+    },
+    authorize(req, res, next) {
+        var token = req.body.token || req.query.token || req.headers['x-access-token']
+
+        if (!token) {
+            res.status(401).json({ message: 'Acesso restrito' })
+        } else {
+            jwt.verify(token, config.password, (error, decoded) => {
+                if (error) {
+                    res.status(401).json({ message: 'Token inválido' })
+                } else {
+                    next()
+                }
+            })
+
+        }
+    },
+    async isAdmin(req, res, next) {
+        var token = req.body.token || req.query.token || req.headers['x-access-token']
+        if (!token) {
+            res.status(401).json({ message: 'Acesso restrito' })
+        } else {
+            jwt.verify(token, global.SALT_KEY, (error, decoded) => {
+                if (error) {
+                    res.status(401).json({ message: 'Token inválido' })
+                } else {
+                    if (decoded.roles.includes('admin')) {
+                        next()
+                    } else {
+                        res.status(403).json({ message: 'Esta funcionalidade é restrita para administradores' })
+
+                    }
+                }
+            })
+        }
+
+    }
+}

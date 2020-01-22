b948492afca46046e541085933d5d95bfd24da24
André Macena <andrmacena_@outlook.com>
2020-01-21 23:11:57 -0300

    Alterado index.js, foi adicionado a conexão para a model Player

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,12 @@ const Sequelize = require('sequelize')
 const dbConfig = require('../config/database')
 
 const User = require('../models/User')
+const Player = require('../models/Player')
 
 const connection = new Sequelize(dbConfig)
 
 User.init(connection)
+Player.init(connection)
 
 module.exports = connection
 

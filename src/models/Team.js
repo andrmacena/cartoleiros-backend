243add08c3084432d1e5f20b92876bc1785bc2e9
André Macena <andrmacena_@outlook.com>
2020-01-23 22:49:20 -0300

    Criado model Team.js

---
+++ src/models/Team.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require('sequelize')
+
+class Team extends Model {
+   static init(connection) {
+      super.init({
+         name: DataTypes.STRING,
+         logo_url: DataTypes.STRING
+      }, { sequelize: connection })
+
+   }
+   //criando relacionamento entre os modelos
+   static associate(models) {
+      this.hasMany(models.Player, { foreignKey: 'player_id', through: 'player_teams', as: 'players' })
+      this.belongsTo(models.User, { foreignKey: 'user_id', as: 'owner' })
+
+   }
+}
+
+
+module.exports = Team

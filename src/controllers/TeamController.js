60b0535bff2350f848e3400a1f85c2d54e4fd5af
André Macena <andrmacena_@outlook.com>
2020-01-23 22:50:56 -0300

    Criado controller TeamController.js

---
+++ src/controllers/TeamController.js
@@ -0,0 +1,12 @@
+const repository = require('../repositories/TeamRepository')
+const validator = require('../validators/FluentValidator')
+
+module.exports = {
+
+   async getAllTeams(req, res, next) {
+
+   },
+   async createTeam(req, res, next) {
+
+   }
+}

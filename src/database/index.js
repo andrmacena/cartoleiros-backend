34c2f53917dcccb0739a790f421cde02cc41d8b8
André Macena <andrmacena_@outlook.com>
2020-01-23 22:51:33 -0300

    Alterado database/index.js, adicionado conexao para o novo model e para as relacoes

--- src/database/index.js
+++ src/database/index.js
@@ -3,12 +3,17 @@ const dbConfig = require('../config/database')
 
 const User = require('../models/User')
 const Player = require('../models/Player')
+const Team = require('../models/Team')
 
 const connection = new Sequelize(dbConfig)
 
 User.init(connection)
 Player.init(connection)
+Team.init(connection)
+
+
+User.associate(connection.models)
+Player.associate(connection.models)
+Team.associate(connection.models)
 
 module.exports = connection
-
-

5e0289228b6ce460fcee3e08b15ed4ce83fa3393
André Macena <andrmacena_@outlook.com>
2020-01-29 22:30:09 -0300

    Alterado TeamRoute.js, adicionado autorização e criação de time com usuario autenticado

--- src/routes/TeamRoute.js
+++ src/routes/TeamRoute.js
@@ -3,10 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 const controller = require('../controllers/TeamController')
+const authService = require('../services/authService')
 
 
 router.get('/', controller.getAllTeams)
-router.post('/', controller.createTeam)
+router.post('/', authService.authorize, controller.createTeam)
 
 
 module.exports = router

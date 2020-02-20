a0fcc60b94582cd8976e60022e4675c93ac3d0ad
André Macena <andrmacena_@outlook.com>
2020-02-19 22:56:20 -0300

    Alterado TeamRepository.js, alterado funcoes com o novo campo player_id e alterada funcao para add players (wip)

--- src/repositories/TeamRepository.js
+++ src/repositories/TeamRepository.js
@@ -4,26 +4,38 @@ const PlayerRepository = require('../repositories/PlayerRepository')
 module.exports = {
 
    async createTeam(data) {
-      const { name, logo_url, user_id } = data
+      const { name, logo_url, user_id, player_id } = data
 
       await Team.create({
          name,
          logo_url,
-         user_id
+         user_id,
+         player_id
       })
    },
    async getTeam(id) {
-
-      const team = await Team.findAll({ where: { user_id: id } })
+      
+      const team = await Team.findOne({ where: { user_id: id } })
 
       return team
    },
 
-   async addPlayerToTeam(player_id){
-      const { id } = player_id
+   async addPlayerToTeam(player_id, data) {
+      const res = await PlayerRepository.validarPlayerID(player_id)
 
-      const player = await PlayerRepository.getById(id)
+      if (!res) {
+         return false
+      }
 
-      return await Team.addPlayer(player)
+      const team = await this.getTeam(data.id)
+
+      Team.create({
+         name: team.name,
+         logo_url: team.logo_url,
+         user_id: team.user_id,
+         player_id: player_id
+      }, { where: { user_id: team.user_id } })
+
+      return true
    }
 }

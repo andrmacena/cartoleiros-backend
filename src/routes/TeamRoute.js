25147af549053e6c4298260ba04aae1528431b54
André Macena <andrmacena_@outlook.com>
2020-02-21 23:02:53 -0300

    Alterado TeamRoute.js, adicionada rota para removao de jogadores do time

--- src/routes/TeamRoute.js
+++ src/routes/TeamRoute.js
@@ -9,6 +9,7 @@ const authService = require('../services/authService')
 router.get('/', authService.authorize, controller.getTeam)
 router.post('/', authService.authorize, controller.createTeam)
 router.post('/:id/player', authService.authorize, controller.addPlayer)
+router.delete('/:id/player', authService.authorize, controller.removePlayer)
 
 
 module.exports = router

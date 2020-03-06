2b740600ef1d2e8a222202daadcf6ae7363f3051
André Macena <andrmacena_@outlook.com>
2020-03-05 22:56:39 -0300

    Alterado login.test.js, refatorado o código para executar exclusao de dados antes de cada teste

--- __tests__/integration/login.test.js
+++ __tests__/integration/login.test.js
@@ -1,25 +1,33 @@
-const request = require('supertest')
-const UserController = require('../../src/controllers/UserController')
-const UserRepository = require('../../src/repositories/UserRepository')
-//const truncate = require('../utils/truncate')
-
 const app = require('../../src/app')
+const UserRepository = require('../../src/repositories/UserRepository')
+
+const truncate = require('../utils/truncate')
+const config = require('../../src/config/database')
+
+//external packages
+const request = require('supertest')
+const md5 = require('md5')
+
 
 describe('Authentication', () => {
-   // beforeEach(async () => {
-   //    await truncate()
-   // })
+   beforeEach(async () => {
+      await truncate()
+   })
 
    it('should return status 201 when user authenticated with valid credentials', async () => {
 
       userReq = {
          name: 'André',
          email: 'andremacena@gmail.com',
-         password: 'andremacena',
+         password: md5('andremacena' + config.password),
          roles: 'user'
       }
 
+      console.log(userReq.password)
+
       const user = await UserRepository.createUser(userReq)
+
+      console.log(user.password)
 
       const response = await request(app)
          .post('/users/authenticate')
@@ -28,9 +36,11 @@ describe('Authentication', () => {
             password: user.password
          })
 
-         console.log(response.statusCode)
+         //TODO receber o password correto, pois está enviando um e retornando outro
 
-      expect(response.statusCode).toBe(201)
+      expect(response.status).toBe(201)
+
+
    })
 
    it('should return status 401 when user authenticated with invalid credentials', async () => {
@@ -50,6 +60,6 @@ describe('Authentication', () => {
             password: 'user.password'
          })
 
-      expect(response.statusCode).toBe(401)
+      expect(response.status).toBe(401)
    })
 })

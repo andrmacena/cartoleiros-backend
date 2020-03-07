5ff863cadbf0c6d02ecdbbd1531fc30e8e0d8161
André Macena <andrmacena_@outlook.com>
2020-03-07 20:19:54 -0300

    Alterado TeamRoute.js, adicionada nova rota para buscar os players do time e alterado rota de todos os times para somente admins

--- src/routes/TeamRoute.js
+++ src/routes/TeamRoute.js
@@ -6,7 +6,8 @@ const controller = require('../controllers/TeamController')
 const authService = require('../services/authService')
 
 
-router.get('/', authService.authorize, controller.getTeam)
+router.get('/', authService.isAdmin, controller.getTeam)
+router.get('/players', authService.authorize, controller.getTeamAndPlayers)
 router.post('/', authService.authorize, controller.createTeam)
 router.post('/:id/player', authService.authorize, controller.addPlayer)
 router.delete('/:id/player', authService.authorize, controller.removePlayer)

59a7818242bddb83c5a23ebe5731d9cca62480ac
André Macena <andrmacena_@outlook.com>
2020-03-07 20:20:48 -0300

    Alterado UserRoute.js, refatorado para limitar a listagem de todos os usuarios para somente admins

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -3,9 +3,10 @@ const express = require('express')
 const router = express.Router()
 
 const controller = require('../controllers/UserController')
+const authService = require('../services/authService')
 
 
-router.get('/', controller.getAllUsers)
+router.get('/', authService.isAdmin, controller.getAllUsers)
 router.post('/', controller.createUser)
 router.post('/authenticate', controller.authenticate)
 

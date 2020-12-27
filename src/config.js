df5ca93702efeff9360c294f07a593650f54ce66
André Macena <andrmacena_@outlook.com>
2020-12-27 18:34:57 -0300

    feat: Adicionado novo template para envio de email para reset de senha

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> seja bem vindo(a) ao Cartoleiros'
+global.EMAIL_TMPL_RESET_PWD = 'Olá, para realizar o reset da sua senha entre <a href="{0}">aqui</a>'
 
 module.exports = {
     sendgridKey: process.env.SENDGRID_KEY,
